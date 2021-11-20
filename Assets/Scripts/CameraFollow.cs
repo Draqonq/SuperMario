ff204348f2d06a076aeb78fba6063e1ec2065b20
PiotrCzerwinski <86910760+PiotrCzerwinski@users.noreply.github.com>
2021-11-20 21:18:59 +0100

    Made camera following mario

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public Vector2 followOffset;
+    private Vector2 threshold;
+    public float speed = 3;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        threshold = calculateThreshold();
+        rb = target.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 follow = target.transform.position;
+        float xDiference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDiference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        Vector3 newPosition = transform.position;
+
+        if (Mathf.Abs(xDiference) >= threshold.x) {
+            newPosition.x = follow.x;
+        }
+
+        if (Mathf.Abs(yDiference) >= threshold.y)
+        {
+            newPosition.y = follow.y;
+        }
+        //camera speed equal to mario movement speed if he is going faster, or deafault if he doesnt
+        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+    }
+
+    private Vector3 calculateThreshold() {
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        t.x -= followOffset.x;
+        t.y -= followOffset.y;
+        return t;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 border = calculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
